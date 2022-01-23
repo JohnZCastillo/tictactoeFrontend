885fd8c87c5bb2db636da7e824236efc9eebe2bb
John Z Castillo <johnzunigacastillo@gmail.com>
2022-01-23 15:56:07 +0800

    add css and layout of board and boxes

---
+++ src/components/Box.js
@@ -0,0 +1,9 @@
+function Box({value}){
+    return(
+        <div className="box wrapper">
+            <h2>{value}</h2>
+        </div>
+    )
+}
+
+export default Box
--- src/index.js
+++ src/index.js
@@ -1,10 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+import Box from './components/Box'
+import './style.css'
 
 ReactDOM.render(
   <React.StrictMode>
-  
+    <div className='wrapper screen'>
+      <div className='board center'>
+         <Box value='yow'/>
+         <Box value='yow'/>
+         <Box value='yow'/>
+         <Box value='yow'/>
+         <Box value='yow'/>
+         <Box value='yow'/>
+         <Box value='yow'/>
+         <Box value='yow'/>
+         <Box value='yow'/>
+      </div>
+    </div>
   </React.StrictMode>,
   document.getElementById('root')
 );
